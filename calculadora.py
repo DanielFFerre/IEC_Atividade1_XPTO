61a166c4ae1478c2d1ec73c83d98de53b3c7b280
Daniel Felipe Ferreira <daniel.ferreira53@fatec.sp.gov.br>
2026-08-19 11:08:30 -0300

    atualizaçao calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,31 +1,37 @@
-##CALCULADORA
 def soma(a,b):
-    return a+ b
+    return a + b
+
 def subtracao(a,b):
     return a - b
+
 def multiplicacao(a,b):
-    return a*b
+    return a * b
+
 def divisao(a,b):
     if b == 0:
         raise ValueError("Divisão por zero não é permitida")
     return a / b
 
-print("soma: + ")
-print("subtracao: - ")
-print("multiplicacao: * ")
-print("divisao: / ")
-print("de um espaço entre os numeros e a operacao ")
-print("Exemplo: 10 + 10")
-a,operacao,b = input("digite a operacao que deseja fazer: ").split()
-a = float(a)
-b = float(b)
-if operacao == "+":
-    print(soma(a,b))
-elif operacao == "-":
-    print(subtracao(a,b))
-elif operacao == "*":
-    print(multiplicacao(a,b))
-elif operacao == "/":
-    print(divisao(a,b))
-else:
-    print("operacao invalida")
+# A mágica acontece aqui:
+if __name__ == "__main__":
+    print("soma: + ")
+    print("subtracao: - ")
+    print("multiplicacao: * ")
+    print("divisao: / ")
+    print("de um espaço entre os numeros e a operacao ")
+    print("Exemplo: 10 + 10")
+    
+    a,operacao,b = input("digite a operacao que deseja fazer: ").split()
+    a = float(a)
+    b = float(b)
+    
+    if operacao == "+":
+        print(soma(a,b))
+    elif operacao == "-":
+        print(subtracao(a,b))
+    elif operacao == "*":
+        print(multiplicacao(a,b))
+    elif operacao == "/":
+        print(divisao(a,b))
+    else:
+        print("operacao invalida")

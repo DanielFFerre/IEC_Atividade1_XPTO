181116c4edfd2c2207641d13b2de5edb9da82ce1
Daniel Felipe Ferreira <daniel.ferreira53@fatec.sp.gov.br>
2026-08-26 09:16:49 -0300

    nome das classes principais alterados

--- test_calculadora.java
+++ test_calculadora.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CalculadoraTest {
+public class test_calculadora {
 
     private final Calculadora calc = new Calculadora();
 

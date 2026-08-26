cc7e3bd7a42c843690cb51ee877e2ca5b205fa98
Daniel Felipe Ferreira <daniel.ferreira53@fatec.sp.gov.br>
2026-08-26 08:41:02 -0300

    criação do arquivo calculadora.java e implementação de funçoes

---
+++ calculadora.java
@@ -0,0 +1,16 @@
+public class Calculadora{
+    public int soma(){
+        return a + b;
+    }
+    public int subtracao(){
+        return a - b;
+    }
+    public int multiplicacao(){
+        return a*b;
+    }
+    public int divisao(){
+        return a/b;
+    }
+
+
+}

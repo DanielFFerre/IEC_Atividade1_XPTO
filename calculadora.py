2c6bcffa420ba99d3b5c4028bb31732f1e9d316c
Daniel Felipe Ferreira <daniel.ferreira53@fatec.sp.gov.br>
2026-08-26 11:17:34 -0300

    alteração na calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,22 @@
-def soma(a,b):
+def soma(a, b):
     return a + b
 
-def subtracao(a,b):
+
+def subtracao(a, b):
     return a - b
 
-def multiplicacao(a,b):
+
+def multiplicacao(a, b):
     return a * b
 
-def divisao(a,b):
+
+def divisao(a, b):
     if b == 0:
         raise ValueError("Divisão por zero não é permitida")
     return a / b
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # pragma: no cover
     print("soma: + ")
     print("subtracao: - ")
     print("multiplicacao: * ")
@@ -20,17 +24,17 @@ if __name__ == "__main__":
     print("de um espaço entre os numeros e a operacao ")
     print("Exemplo: 10 + 10")
     
-    a,operacao,b = input("digite a operacao que deseja fazer: ").split()
+    a, operacao, b = input("digite a operacao que deseja fazer: ").split()
     a = float(a)
     b = float(b)
     
     if operacao == "+":
-        print(soma(a,b))
+        print(soma(a, b))
     elif operacao == "-":
-        print(subtracao(a,b))
+        print(subtracao(a, b))
     elif operacao == "*":
-        print(multiplicacao(a,b))
+        print(multiplicacao(a, b))
     elif operacao == "/":
-        print(divisao(a,b))
+        print(divisao(a, b))
     else:
         print("operacao invalida")

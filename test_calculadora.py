d6275991c30df75de06a4aa67649b6456ec8833d
Daniel Felipe Ferreira <Danielseujooj@gmail.com>
2026-08-26 18:34:14 -0300

    atualização no test_calculadora

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,15 +1,19 @@
 import pytest
 from calculadora import soma, subtracao, multiplicacao, divisao
 
+
 def test_soma():
     assert soma(10, 5) == 15
     assert soma(-2, 2) == 0
 
+
 def test_subtracao():
     assert subtracao(10, 5) == 5
 
+
 def test_multiplicacao():
     assert multiplicacao(3, 4) == 12
+
 
 def test_divisao():
     assert divisao(10, 2) == 5

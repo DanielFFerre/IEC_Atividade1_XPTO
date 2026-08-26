8602eaed0ba6e0d81d8e1a9bb89c85d153a91e47
Daniel Felipe Ferreira <daniel.ferreira53@fatec.sp.gov.br>
2026-08-26 08:57:57 -0300

    o arquivo teste_calculadora.java foi criado, e inserido informaçoes para o teste

---
+++ test_calculadora.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculadoraTest {
+
+    private final Calculadora calc = new Calculadora();
+
+    @Test
+    public void testSoma() {
+        assertEquals(5, calc.soma(2, 3));
+    }
+
+    @Test
+    public void testSubtracao() {
+        assertEquals(1, calc.subtracao(4, 3));
+    }
+
+    @Test
+    public void testMultiplicacao() {
+        assertEquals(6, calc.multiplicacao(2, 3));
+    }
+
+    @Test
+    public void testDivisao() {
+        assertEquals(2, calc.divisao(6, 3));
+    }
+}
